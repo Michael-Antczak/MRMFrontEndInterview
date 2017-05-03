149a4f1580bf814acc26f2ebca052701d7ba9e3a
Michael Antczak <Michael.Antczak@gmail.com>
2017-05-03 17:23:44 +0100

    add test1 function

--- app/test1.js
+++ app/test1.js
@@ -1,11 +1,50 @@
 export function filterNames(names, startsWith){
+  
+    // add a test for the startsWith to be a string
 
+    // array to hold the result of filtering
+    var result = [];
+
+    // check every element of array for the condition
+    // if met => add to result
+    names.filter(function(elem, index, arr) {
+	
+	    if (elem.startsWith(startsWith)) result.push(elem);
+
+    });
+
+    return result;
 }
 
 export function objectFilter(objects, key, searchString) {
+  
+    // objects is an array of objects
 
+    var result = [];
+
+    objects.filter(function(elem, index, arr) {
+
+        if (elem[key] === searchString) result.push(elem);
+
+    })
+
+    return result;
 }
 
 export function compoundObjectFilter(objects, keysString, searchString) {
+  
+  // objects is an array of objects
+
+    var keys = keysString.split('.');
+
+    var result = [];
+
+    objects.filter(function(elem, index, arr) {
+
+        if (elem[keys[0]][keys[1]] === searchString) result.push(elem);
+
+    })
+
+    return result;
 
 }
