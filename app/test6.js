c50b95334e308161ad3b6d1d71a417294b6c75af
Michael Antczak <Michael.Antczak@gmail.com>
2017-05-04 14:56:32 +0100

    Done test 6

--- app/test6.js
+++ app/test6.js
@@ -10,12 +10,42 @@ class Greeting {
 // Add your new classes here
 export class English extends Greeting {
 
+  constructor(name) {
+        super(name);
+    }
+
+  sayHello() {
+    var hello = 'Hello ' + this.name;
+    return hello;
+  
+  }
+
 }
 
 export class French extends Greeting {
 
+   constructor(name) {
+        super(name);
+    }
+
+  sayHello() {
+    var hello = 'Bonjour ' + this.name;
+    return hello;
+  
+  }
+
 }
 
 export class Spanish extends Greeting {
+
+  constructor(name) {
+        super(name);
+    }
+
+  sayHello() {
+    var hello = 'Hola ' + this.name;
+    return hello;
+  
+  }
 
 }
