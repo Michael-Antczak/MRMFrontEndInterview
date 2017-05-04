d3e1254f8066a87c9f634929db3446d2827edf7d
Michael Antczak <Michael.Antczak@gmail.com>
2017-05-04 16:07:06 +0100

    Done test 7

--- app/test7.js
+++ app/test7.js
@@ -1,7 +1,10 @@
 export default function test7(string){
   //Write your solution here
 
-
+  String.prototype.reverse = function()
+    {
+        return this.split('').reverse().join('');
+    }
   //
   return string.reverse();
 }
