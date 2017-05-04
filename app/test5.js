5b1a4acc1a4bbd1479f07fe9d05299855b71048e
Michael Antczak <Michael.Antczak@gmail.com>
2017-05-04 14:30:00 +0100

    Done test 5

--- app/test5.js
+++ app/test5.js
@@ -4,8 +4,49 @@ export function groupBy(people, key) {
 
 export function groupBySex(people) {
 
+    var result = {
+        male: [],
+        female: []
+    };
+
+    people.map(function(elem, item) {
+
+        if (elem.sex == 'male') {
+            result.male.push(elem);
+        } else if (elem.sex == 'female') {
+            result.female.push(elem);
+        }
+    });
+
+    return result;
+
 }
 
 export function groupByYearThenSex(people) {
+
+    // get all the values of year
+    var bornYear = [];
+    people.map(function(elem, index) {
+        if(bornYear.includes(elem.born) == false) {
+            bornYear.push(elem.born);
+        }
+    });
+
+    // initialise the result object
+    var result = {};
+    bornYear.map(function(elem, item) {
+        result[elem] = {
+            male: [],
+            female: []
+        };
+    });
+
+    // iterate over people add push them to the appropriate 
+    // place in the result object
+    people.map(function(elem, index) {
+        result[elem.born][elem.sex].push(elem);
+    });
+
+    return result;
 
 }
